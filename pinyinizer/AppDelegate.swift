8a2c007d7654648c374e55fbc00ba330c3454132
Shohei Aoki <shoaok@gmail.com>
2015-05-16 15:07:58 +0900

    Initial Commit

---
+++ pinyinizer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  pinyinizer
+//
+//  Created by shohei on 5/16/15.
+//  Copyright (c) 2015 shohei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
