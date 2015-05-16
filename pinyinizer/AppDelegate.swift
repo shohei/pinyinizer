972bc99b0260dfe2d954b811a59149472db93c54
Shohei Aoki <shoaok@gmail.com>
2015-05-16 15:20:14 +0900

    initial commit

--- pinyinizer/AppDelegate.swift
+++ pinyinizer/AppDelegate.swift
@@ -10,12 +10,49 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
+    @IBOutlet weak var theLabel: NSTextField!
+    @IBOutlet weak var theButton: NSButton!
     @IBOutlet weak var window: NSWindow!
+    
+    
+    var buttonPresses = 0;
+    
+    var statusBar = NSStatusBar.systemStatusBar()
+    var statusBarItem : NSStatusItem = NSStatusItem()
+    var menu: NSMenu = NSMenu()
+    var menuItem : NSMenuItem = NSMenuItem()
 
+    override func awakeFromNib() {
+        theLabel.stringValue = "You've pressed the button \n \(buttonPresses) times!"
+        
+        //Add statusBarItem
+        statusBarItem = statusBar.statusItemWithLength(-1)
+        statusBarItem.menu = menu
+        statusBarItem.title = "Presses"
+        
+        //Add menuItem to menu
+        menuItem.title = "Clicked"
+        menuItem.action = Selector("setWindowVisible:")
+        menuItem.keyEquivalent = ""
+        menu.addItem(menuItem)
+    }
+    
 
+    @IBAction func buttonPressed(sender: NSButton) {
+        buttonPresses+=1
+        theLabel.stringValue = "You've pressed the button \n \(buttonPresses) times!"
+        menuItem.title = "Clicked \(buttonPresses)"
+        statusBarItem.title = "Presses \(buttonPresses)"
+    }
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+          self.window!.orderOut(self)
+    }
+    
+    
+    func setWindowVisible(sender: AnyObject){
+        self.window!.orderFront(self)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
